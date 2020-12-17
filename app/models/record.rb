25ad451fa7e29d435cd1a4703be544f3ae65d020
tech-o-zukky <tech.o.zukky@gmail.com>
2020-12-17 19:04:57 +0900

    Recordモデル：アソシエーション設定

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,21 @@
 class Record < ApplicationRecord
+    # ActiveHash用アソシエーション
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :prefecture
+    belongs_to_active_hash :tide
+    belongs_to_active_hash :water_quality
+    belongs_to_active_hash :wave
+    belongs_to_active_hash :weather
+    belongs_to_active_hash :wind
+  
+    # アソシエーション
+    belongs_to :user
+  
+    # バリデーション
+    validates :title,            presence: true
+    validates :date,             presence: true
+    validates :content,          presence: true
+
+    # ActiveHash利用カラムのバリデーション
+    validates :prefecture_id,    presence: true, numericality: { other_than: 0, message: "を選択してください。" }
 end
