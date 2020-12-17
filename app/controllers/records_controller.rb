bcbd14b096d690232e48d66bcf4fdbd00430fc7a
tech-o-zukky <tech.o.zukky@gmail.com>
2020-12-17 19:39:03 +0900

    records#newの処理追加

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,25 @@
 class RecordsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+  end
+
+  def new
+    @fish_record = FishRecord.new
+  end
+
+  def create
+  end
+
+  private
+
+  def fish_record_params
+    params.require(:fish_record).permit(
+      :title, :date, :prefecture_id, :area, :place,
+      :timing1, :timing2, :timing3, :timing4, :timing5,
+      :timing6, :timing7, :weather_id, :wind_id, :temperature,
+      :water_temperature, :wave_id,  :water_quality_id,  :tide_id,  
+      :content, fish_name: [], fish_count: [], images: []
+    ).merge(user_id: current_user.id)
   end
 end
