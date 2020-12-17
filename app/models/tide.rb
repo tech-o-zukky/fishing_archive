109fe4f273a18293ce448535a962473273e7e07f
tech-o-zukky <tech.o.zukky@gmail.com>
2020-12-17 18:56:01 +0900

    ActiveHashの値を追記

--- app/models/tide.rb
+++ app/models/tide.rb
@@ -1,2 +1,13 @@
-class Tide < ApplicationRecord
+class Tide < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '--' }, 
+    { id: 1, name: '大潮' }, 
+    { id: 2, name: '中潮' }, 
+    { id: 3, name: '小潮' }, 
+    { id: 4, name: '長潮' }, 
+    { id: 5, name: '若潮' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :records
 end
