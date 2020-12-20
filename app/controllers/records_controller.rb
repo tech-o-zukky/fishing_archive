9e03054a9732c15f4a2671a444e3774960ed9cf1
tech-o-zukky <tech.o.zukky@gmail.com>
2020-12-20 18:49:43 +0900

    edit, createアクションとupdate処理追加

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -26,6 +26,23 @@ class RecordsController < ApplicationController
     end
   end
 
+  def edit
+    @fish_record = FishRecord.new
+    select_fish_record(@fish_record, params[:id])
+  end
+
+  def update
+    @fish_record = FishRecord.new
+    if @fish_record.update(fish_record_params, params[:id])
+      redirect_to root_path
+    else
+      #戻った時ひとつの欄にまとめて記述されてしまうため、一旦クリアしておく
+      @fish_record.fish_name = ""
+      @fish_record.fish_count = ""
+      render :new
+    end
+  end
+
   private
 
   def fish_record_params
@@ -37,4 +54,40 @@ class RecordsController < ApplicationController
       :content, fish_name: [], fish_count: [], images: []
     ).merge(user_id: current_user.id)
   end
+
+  def select_fish_record(fishrecord, id)
+    getdata = Record.find(params[:id])
+
+    fishrecord.title = getdata.title
+    fishrecord.date = getdata.date
+    fishrecord.prefecture_id = getdata.prefecture_id
+    fishrecord.area = getdata.area
+    fishrecord.place = getdata.place
+    fishrecord.timing1 = getdata.timing1
+    fishrecord.timing2 = getdata.timing2
+    fishrecord.timing3 = getdata.timing3
+    fishrecord.timing4 = getdata.timing4
+    fishrecord.timing5 = getdata.timing5
+    fishrecord.timing6 = getdata.timing6
+    fishrecord.timing7 = getdata.timing7
+    fishrecord.weather_id = getdata.weather_id
+    fishrecord.wind_id = getdata.wind_id
+    fishrecord.temperature = getdata.temperature
+    fishrecord.water_temperature = getdata.water_temperature
+    fishrecord.wave_id = getdata.wave_id
+    fishrecord.water_quality_id = getdata.water_quality_id
+    fishrecord.tide_id = getdata.tide_id
+    fishrecord.content = getdata.content
+    fishrecord.fish_name = []
+    fishrecord.fish_count = []
+
+    #魚種情報の取得
+    if getdata.fish_kinds.count > 0
+      getdata.fish_kinds.each do | fish_kind |
+        fishrecord.fish_name << fish_kind.fish_name
+        fishrecord.fish_count << fish_kind.fish_count
+      end
+    end
+  end
+
 end
--- app/models/fish_record.rb
+++ app/models/fish_record.rb
@@ -32,7 +32,29 @@ class FishRecord
     #魚種の数だけfish_kindsに登録
     fish_name.length.times do |i|
       if fish_name[i] != "" && fish_count[i] != ""
-        FishKind.create(fish_name: fish_name[i], fish_count: fish_count[i], record_id: record.id)
+        FishKind.create(fish_name: fish_name[i], fish_count: fish_count[i].to_i, record_id: record.id)
+      end
+    end
+  end
+
+  def update(item, id)
+    #Recordのアップデート
+    record = Record.find(id)
+
+    record.update(
+      title: item[:title], date: item[:date], prefecture_id: item[:prefecture_id], area: item[:area], place: item[:place],
+      timing1: item[:timing1], timing2: item[:timing2], timing3: item[:timing3], timing4: item[:timing4], timing5: item[:timing5],
+      timing6: item[:timing6], timing7: item[:timing7], weather_id: item[:weather_id], wind_id: item[:wind_id], temperature: item[:temperature],
+      water_temperature: item[:water_temperature], wave_id: item[:wave_id],  water_quality_id: item[:water_quality_id], tide_id: item[:tide_id], content: item[:content],
+      images: item[:images], user_id: item[:user_id]
+    )
+    
+    #魚種の数だけfish_kindsに登録。一度、古いレコードを削除してcreateすることにした
+    FishKind.destroy_by(record_id: record.id)
+
+    item[:fish_name].length.times do |i|
+      if item[:fish_name][i] != "" && item[:fish_count][i] != ""
+        FishKind.create(fish_name: item[:fish_name][i], fish_count: item[:fish_count][i].to_i, record_id: record.id)
       end
     end
   end
