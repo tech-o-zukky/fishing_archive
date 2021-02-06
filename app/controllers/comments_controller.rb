b5921167cdc170476294ff5d1f4335b5844a7a64
tech-o-zukky <tech.o.zukky@gmail.com>
2021-02-06 15:09:59 +0900

    Commentコントローラ:createアクション作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+
+  def create
+    Comment.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment_text).merge(user_id: current_user.id, recored_id: params[:record_id])
+  end
 end
