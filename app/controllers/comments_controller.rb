7c5a26e1b2d645683e7b9f5729a30e3cd1619cd8
tech-o-zukky <tech.o.zukky@gmail.com>
2021-02-07 15:32:59 +0900

    Comment：一覧表示と投稿の処理修正
    ActionCableを利用

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
-    Comment.create(comment_params)
+    @record = Record.find(params[:record_id])
+    @comment = @record.comments.build(comment_params)
+    if @comment.save
+      ActionCable.server.broadcast 'comment_channel', content: @comment, user: @comment.user, date: l(@comment.created_at, format: :long)
+    end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment_text).merge(user_id: current_user.id, recored_id: params[:record_id])
+    params.require(:comment).permit(:comment_text, :record_id, :user_id).merge(user_id: current_user.id)
   end
 end
