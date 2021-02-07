2e1af6e2751b574390ce7c02bb3787ba26f181a1
tech-o-zukky <tech.o.zukky@gmail.com>
2021-02-07 14:48:07 +0900

    routes: コメントメモ追記

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'records#index'
   resources :records, only: [ :index, :new, :create, :show, :edit, :update ] do
-    resources :comments, only: :create
+    resources :comments, only: :create  # add コメント機能追加
   end
 end
